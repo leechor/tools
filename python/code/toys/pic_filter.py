a3b7bc36f38aaeb8b1f47c09395d8189cf75a8dc
licho <chinabhsun@gmail.com>
2019-05-05 23:07:27 +0800

    测试可用

--- python/code/toys/pic_filter.py
+++ python/code/toys/pic_filter.py
@@ -4,27 +4,34 @@ import os
 import shutil
 import sys
 
-from termcolor import cprint
+import pathlib
+from datetime import datetime
+
 import imagehash as imagehash
+import patoolib
 from PIL import Image, ExifTags
 from apscheduler.schedulers.blocking import BlockingScheduler
+from patoolib.util import PatoolError
+
+
+def is_image(file_name):
+    # List mime types fully supported by Pillow
+    full_supported_formats = ['gif', 'jp2', 'jpeg', 'jpg', 'pcx', 'png', 'tiff', 'x-ms-bmp',
+                              'x-portable-pixmap', 'x-xbitmap']
+    try:
+        ext = os.path.splitext(file_name)[-1][1:]
+        return ext.lower() in full_supported_formats
+    except IndexError:
+        return False
 
 
 def get_all_files(path):
-    def is_image(file_name):
-        # List mime types fully supported by Pillow
-        full_supported_formats = ['gif', 'jp2', 'jpeg', 'jpg', 'pcx', 'png', 'tiff', 'x-ms-bmp',
-                                  'x-portable-pixmap', 'x-xbitmap']
-        try:
-            ext = os.path.splitext(file_name)[-1][1:]
-            return ext in full_supported_formats
-        except IndexError:
-            return False
-
     for dir_path, dir_names, filenames in os.walk(path):
+        for d_name in dir_names:
+            d = os.path.join(dir_path, d_name)
+            if len(os.listdir(d)) == 0 and d != path:
+                os.rmdir(d + os.sep)
         for name in filenames:
-            if not is_image(name):
-                continue
             yield os.path.join(dir_path, name)
 
 
@@ -78,23 +85,50 @@ def hash_file(file):
 
 
 def job(source_path_):
-    for of in get_all_files(source_path_):
-        h = hash_file(of)
+    for of_ in get_all_files(source_path_):
+        try:
+            if not is_image(of_):
+                try:
+                    format, compression = patoolib.get_archive_format(of_)
+                    target = patoolib.extract_archive(of_, outdir=source_path_)
+                    os.remove(of_)
+                    continue
+                except PatoolError as ex:
+                    os.remove(of_)
+                    continue
+        except Exception as ex:
+            continue
+
+        h = hash_file(of_)
 
         if h is None:
             continue
 
-        if h[1] in h_dict.keys():
-            try:
-                os.remove(of)
-            except:
-                continue
-        else:
-            try:
-                shutil.move(h[0], output)
-                h_dict[h[1]] = h[0]
-            except:
-                continue
+        h_dict[h[1]] = h[0]
+        key_list = list(h_dict.keys())
+
+        i = 0
+        while i < len(key_list):
+            ha = key_list[i]
+            i += 1
+            s = [1 for a, b in zip(ha, h[1]) if a != b]
+            if len(s) < 5:  # 照片相同
+                try:
+                    os.remove(of_)
+                    break
+                except Exception as ex:
+                    print(str(ex))
+            else:
+                try:
+                    if not os.path.exists(output):
+                        os.mkdir(output)
+                    dt = datetime.now()
+                    fn = os.path.basename(h[0])
+                    shutil.move(h[0], f'{output}\\{fn}_{dt.strptime("%Y%m%d%H%M%S")}')
+                    key_list.append(h[1])
+                    break
+                except Exception as ex:
+                    print(str(ex))
 
 
 if __name__ == '__main__':
@@ -123,4 +157,5 @@ if __name__ == '__main__':
         if h is not None:
             h_dict[h[1]] = h[0]
 
+    # job(source_path)
     sched.start()
